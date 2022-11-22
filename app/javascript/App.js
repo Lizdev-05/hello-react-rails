8aa98e04857d7b2396bd1d88e938dbd4ac6bab3a
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-11-22 14:05:55 +0100

    Update

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -4,10 +4,10 @@ import Greeting from "./pages/Greeting";
 
 const App = () => {
   return <>
+   <h2>Greeting App </h2>
     <Routes>
       <Route path="/" element={<Greeting/>}/>
     </Routes>
-    <h2>Treat me well </h2>
   </>
 }
 
